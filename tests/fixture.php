9e602f6da6af9db5f657121e9f4f8fe88c01696a
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-12-22 18:41:31 +0100

    replace xml fixture

---
+++ tests/fixture.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'app_news' => [
+        ['id' => 1, 'title' => 'foo', 'content' => 'bar', 'tags' => json_encode(['foo', 'bar']), 'date' => '2015-02-27 19:59:15'],
+        ['id' => 2, 'title' => 'bar', 'content' => 'foo', 'tags' => json_encode(['foo']), 'date' => '2015-02-27 19:59:15'],
+    ]
+];
