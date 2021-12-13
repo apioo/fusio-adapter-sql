2870c24864704dd0e2dc704914d711b81b061c5e
Mateusz Knapik <mateuszknapik@gmail.com>
2021-12-13 10:44:38 +0100

    Add new fields
    This commit adds the possibility to use custom database driver, as well as passing custom options for the driver.

--- src/Connection/Sql.php
+++ src/Connection/Sql.php
@@ -57,11 +57,32 @@ class Sql implements ConnectionInterface, PingableInterface
             'driver'   => $config->get('type'),
         );
 
+        if ($config->get('type') == 'other' && !empty($config->get('driverClass'))) {
+            unset($params['driver']);
+            $params['driverClass'] = $config->get('driverClass');
+        }
+        
+        if ($config->get('port') != '') {
+        	$params['port'] = $config->get('port');
+        }
+
         if ($config->get('type') == 'pdo_mysql') {
             $params['charset'] = 'utf8';
             $params['driverOptions'] = [
                 \PDO::ATTR_EMULATE_PREPARES => false,
             ];
+        }
+
+        parse_str($config->get('customOptions'), $customOptions);
+        $params = array_merge($params, $customOptions);
+
+        parse_str($config->get('driverOptions'), $driverOptions);
+        if (!empty($driverOptions)) {
+            if (!empty($params['driverOptions'])) {
+                $params['driverOptions'] = array_merge($params['driverOptions'], $driverOptions);
+            } else {
+                $params['driverOptions'] = $driverOptions;
+            }
         }
 
         return DriverManager::getConnection($params);
@@ -75,13 +96,18 @@ class Sql implements ConnectionInterface, PingableInterface
             'sqlsrv'      => 'Microsoft SQL Server',
             'oci8'        => 'Oracle Database',
             'sqlanywhere' => 'SAP Sybase SQL Anywhere',
+            'other'       => 'Custom driver class',
         );
 
         $builder->add($elementFactory->newSelect('type', 'Type', $types, 'The driver which is used to connect to the database'));
+        $builder->add($elementFactory->newInput('driverClass', 'Driver class', 'text', 'Custom driver class name'));
         $builder->add($elementFactory->newInput('host', 'Host', 'text', 'The IP or hostname of the database server'));
+        $builder->add($elementFactory->newInput('port', 'Port', 'number', 'The port used to connect to the database server'));
         $builder->add($elementFactory->newInput('username', 'Username', 'text', 'The name of the database user'));
         $builder->add($elementFactory->newInput('password', 'Password', 'password', 'The password of the database user'));
         $builder->add($elementFactory->newInput('database', 'Database', 'text', 'The name of the database which is used upon connection'));
+        $builder->add($elementFactory->newInput('customOptions', 'Custom options', 'text', 'Custom options for this connection, like charset etc. Formatted as URL arguments'));
+        $builder->add($elementFactory->newInput('driverOptions', 'Driver options', 'text', 'Additional options of this connection, specific to the driver. Formatted as URL arguments'));
     }
 
     public function ping($connection)
